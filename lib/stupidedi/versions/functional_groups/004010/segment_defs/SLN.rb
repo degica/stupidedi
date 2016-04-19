b96864dbcd991858e01c8aee4ed88f91ffd83cd9
Chris Salzberg <csalzberg@degica.com>
2016-04-19 17:32:51 +0900

    Required -> Mandatory

--- lib/stupidedi/versions/functional_groups/004010/segment_defs/SLN.rb
+++ lib/stupidedi/versions/functional_groups/004010/segment_defs/SLN.rb
@@ -10,12 +10,12 @@ module Stupidedi
 
           SLN = s::SegmentDef.build(:SLN, "Subline Item Details",
             "To specify product subline detail item data",
-            e::E350 .simple_use(r::Required,   s::RepeatCount.bounded(1)),
-            e::E662 .simple_use(r::Required,   s::RepeatCount.bounded(1)),
-            e::E380 .simple_use(r::Required,   s::RepeatCount.bounded(1)),
-            e::E355 .simple_use(r::Required,   s::RepeatCount.bounded(1)),
-            e::E235 .simple_use(r::Required,   s::RepeatCount.bounded(1)),
-            e::E234 .simple_use(r::Required,   s::RepeatCount.bounded(1)))
+            e::E350 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E662 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E380 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E355 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E235 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E234 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)))
         end
       end
     end
